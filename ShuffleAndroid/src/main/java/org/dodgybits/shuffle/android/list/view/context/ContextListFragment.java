e182a01e62113df41f46dada314bb9d26a0c8574
Andy Bryant <andybryant@gmail.com>
2015-07-05 21:43:03 +1000

    recyclerview for context list

--- ShuffleAndroid/src/main/java/org/dodgybits/shuffle/android/list/view/context/ContextListFragment.java
+++ ShuffleAndroid/src/main/java/org/dodgybits/shuffle/android/list/view/context/ContextListFragment.java
@@ -3,34 +3,47 @@ package org.dodgybits.shuffle.android.list.view.context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.v7.view.*;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.util.SparseIntArray;
 import android.view.*;
+import android.view.ActionMode;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import com.bignerdranch.android.multiselector.ModalMultiSelectorCallback;
+import com.bignerdranch.android.multiselector.MultiSelector;
+import com.bignerdranch.android.multiselector.SingleSelector;
 import com.google.inject.Inject;
 import org.dodgybits.android.shuffle.R;
 import org.dodgybits.shuffle.android.core.event.*;
 import org.dodgybits.shuffle.android.core.listener.CursorProvider;
+import org.dodgybits.shuffle.android.core.model.Context;
 import org.dodgybits.shuffle.android.core.model.Id;
+import org.dodgybits.shuffle.android.core.model.Project;
 import org.dodgybits.shuffle.android.core.model.persistence.ContextPersister;
 import org.dodgybits.shuffle.android.core.model.persistence.TaskPersister;
 import org.dodgybits.shuffle.android.core.view.Location;
 import org.dodgybits.shuffle.android.core.view.ViewMode;
 import org.dodgybits.shuffle.android.list.event.UpdateContextDeletedEvent;
+import org.dodgybits.shuffle.android.list.event.UpdateProjectDeletedEvent;
 import org.dodgybits.shuffle.android.list.model.ListQuery;
+import org.dodgybits.shuffle.android.list.view.AbstractCursorAdapter;
+import org.dodgybits.shuffle.android.list.view.SelectableHolderImpl;
+import org.dodgybits.shuffle.android.list.view.project.ProjectListItem;
 import org.dodgybits.shuffle.android.roboguice.RoboAppCompatActivity;
 import roboguice.event.EventManager;
 import roboguice.event.Observes;
+import roboguice.fragment.RoboFragment;
 import roboguice.fragment.RoboListFragment;
+import roboguice.inject.ContextScopedProvider;
 
-public class ContextListFragment extends RoboListFragment {
+import java.util.List;
+
+public class ContextListFragment extends RoboFragment {
     private static final String TAG = "ContextListFragment";
-    
-    /** Argument name(s) */
-    private static final String BUNDLE_LIST_STATE = "ContextListFragment.state.listState";
 
-    @Inject
-    private ContextListAdaptor mListAdapter;
 
     @Inject
     private TaskPersister mTaskPersister;
@@ -39,19 +52,97 @@ public class ContextListFragment extends RoboListFragment {
     private ContextPersister mContextPersister;
 
     @Inject
+    private ContextScopedProvider<ContextListItem> mContextListItemProvider;
+
+    @Inject
     private EventManager mEventManager;
 
     @Inject
     private CursorProvider mCursorProvider;
 
-    private Parcelable mSavedListState;
-
     private Cursor mCursor;
+    private RecyclerView mRecyclerView;
+    private ContextListAdapter mListAdapter;
+    private MultiSelector mMultiSelector = new SingleSelector();
+    private android.support.v7.view.ActionMode mActionMode = null;
+    private ModalMultiSelectorCallback mEditMode = new ModalMultiSelectorCallback(mMultiSelector) {
+
+        @Override
+        public boolean onCreateActionMode(android.support.v7.view.ActionMode actionMode, Menu menu) {
+            super.onCreateActionMode(actionMode, menu);
+            getActivity().getMenuInflater().inflate(R.menu.context_list_context_menu, menu);
+
+            String entityName = getString(R.string.context_name);
+            List<Integer> positions = mMultiSelector.getSelectedPositions();
+            boolean isDeleted = false;
+            if (positions != null && !positions.isEmpty() && mCursor != null) {
+                int position = positions.get(0);
+                Context context = mListAdapter.readContext(position);
+                isDeleted = context.isDeleted();
+            }
+
+            MenuItem deleteMenu = menu.findItem(R.id.action_delete);
+            deleteMenu.setVisible(!isDeleted);
+            deleteMenu.setTitle(getString(R.string.menu_delete_entity, entityName));
+
+            MenuItem undeleteMenu = menu.findItem(R.id.action_undelete);
+            undeleteMenu.setVisible(isDeleted);
+            undeleteMenu.setTitle(getString(R.string.menu_undelete_entity, entityName));
+
+            return true;
+        }
+
+        @Override
+        public boolean onActionItemClicked(android.support.v7.view.ActionMode actionMode, MenuItem menuItem) {
+            int position = mMultiSelector.getSelectedPositions().get(0);
+            Id contextId = mListAdapter.readContext(position).getLocalId();
+
+            switch (menuItem.getItemId()) {
+                case R.id.action_edit:
+                    mEventManager.fire(new NavigationRequestEvent(
+                            Location.editContext(contextId)));
+                    mActionMode.finish();
+                    return true;
+
+                case R.id.action_delete:
+                    mEventManager.fire(new UpdateContextDeletedEvent(contextId, true));
+                    mActionMode.finish();
+                    return true;
+
+                case R.id.action_undelete:
+                    mEventManager.fire(new UpdateContextDeletedEvent(contextId, false));
+                    mActionMode.finish();
+                    return true;
+            }
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(android.support.v7.view.ActionMode actionMode) {
+            super.onDestroyActionMode(actionMode);
+            mMultiSelector.clearSelections();
+            mActionMode = null;
+        }
+    };
+
+    /**
+     * When creating, retrieve this instance's number from its arguments.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup) super.onCreateView(inflater, container, savedInstanceState);
-        inflater.inflate(R.layout.fab, root, true);
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.recycler_view, container, false);
+        mRecyclerView = (RecyclerView) root.findViewById(R.id.recycler_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(layoutManager);
+        mListAdapter = new ContextListAdapter();
+        mRecyclerView.setAdapter(mListAdapter);
         return root;
     }
 
@@ -59,22 +150,7 @@ public class ContextListFragment extends RoboListFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        final ListView lv = getListView();
-        lv.setItemsCanFocus(false);
-        lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        registerForContextMenu(lv);
-
-        setEmptyText(getString(R.string.no_contexts));
-
-        if (savedInstanceState != null) {
-            // Fragment doesn't have this method.  Call it manually.
-            restoreInstanceState(savedInstanceState);
-            restoreListState();
-        }
-
         updateCursor();
-        Log.d(TAG, "-onActivityCreated");
-
         getView().findViewById(R.id.fab).setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -84,14 +160,26 @@ public class ContextListFragment extends RoboListFragment {
                     }
                 }
         );
+
+        if (mMultiSelector != null) {
+            if (savedInstanceState != null) {
+                mMultiSelector.restoreSelectionStates(savedInstanceState.getBundle(TAG));
+            }
+
+            if (mMultiSelector.isSelectable()) {
+                if (mEditMode != null) {
+                    mEditMode.setClearOnPrepare(false);
+                    mActionMode = getRoboAppCompatActivity().startSupportActionMode(mEditMode);
+                }
+            }
+        }
     }
 
     @Override
-    public void onPause() {
-        mSavedListState = getListView().onSaveInstanceState();
-        super.onPause();
-
-        Log.d(TAG, "-onPause");
+    public void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "Saving state");
+        outState.putBundle(TAG, mMultiSelector.saveSelectionStates());
+        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -99,36 +187,6 @@ public class ContextListFragment extends RoboListFragment {
         super.onResume();
 
         refreshChildCount();
-    }
-
-    /**
-     * Called when a context is clicked.
-     */
-    @Override
-    public void onListItemClick(ListView parent, View view, int position, long id) {
-        Location location = Location.viewTaskList(ListQuery.context, Id.NONE, Id.create(id));
-        mEventManager.fire(new NavigationRequestEvent(location));
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater inflater = getActivity().getMenuInflater();
-        inflater.inflate(R.menu.context_list_context_menu, menu);
-
-        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-        Cursor cursor = (Cursor) getListAdapter().getItem(info.position);
-        org.dodgybits.shuffle.android.core.model.Context context = mContextPersister.read(cursor);
-
-        String entityName = getString(R.string.context_name);
-
-        MenuItem deleteMenu = menu.findItem(R.id.action_delete);
-        deleteMenu.setVisible(!context.isDeleted());
-        deleteMenu.setTitle(getString(R.string.menu_delete_entity, entityName));
-
-        MenuItem undeleteMenu = menu.findItem(R.id.action_undelete);
-        undeleteMenu.setVisible(context.isDeleted());
-        undeleteMenu.setTitle(getString(R.string.menu_undelete_entity, entityName));
     }
 
     @Override
@@ -177,22 +235,7 @@ public class ContextListFragment extends RoboListFragment {
 
         Log.d(TAG, "Swapping cursor and setting adapter");
         mCursor = cursor;
-
-        mListAdapter.swapCursor(cursor);
-        setListAdapter(mListAdapter);
-
-        restoreListState();
-    }
-
-    private void restoreListState() {
-        if (getActivity() != null && getListAdapter() != null) {
-            // Restore the state -- this step has to be the last, because Some of the
-            // "post processing" seems to reset the scroll position.
-            if (mSavedListState != null) {
-                getListView().onRestoreInstanceState(mSavedListState);
-                mSavedListState = null;
-            }
-        }
+        mListAdapter.changeCursor(cursor);
     }
 
     private void onTaskCountCursorLoaded(@Observes ContextTaskCountLoadedEvent event) {
@@ -201,7 +244,7 @@ public class ContextListFragment extends RoboListFragment {
             getActivity().runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    getListView().invalidateViews();
+                    mListAdapter.notifyDataSetChanged();
                 }
             });
         }
@@ -211,13 +254,79 @@ public class ContextListFragment extends RoboListFragment {
         return (RoboAppCompatActivity) getActivity();
     }
 
-    void restoreInstanceState(Bundle savedInstanceState) {
-        mSavedListState = savedInstanceState.getParcelable(BUNDLE_LIST_STATE);
-    }
-
-
     private void refreshChildCount() {
         mEventManager.fire(new LoadCountCursorEvent(ViewMode.CONTEXT_LIST));
     }
 
+    public class ContextHolder extends SelectableHolderImpl implements
+            View.OnClickListener, View.OnLongClickListener {
+
+        ContextListItem mContextListItem;
+        Context mContext;
+
+        public ContextHolder(ContextListItem contextListItem) {
+            super(contextListItem, mMultiSelector);
+
+            mContextListItem = contextListItem;
+            mContextListItem.setOnClickListener(this);
+            mContextListItem.setLongClickable(true);
+            mContextListItem.setOnLongClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (!mMultiSelector.tapSelection(this)) {
+                if (mContext != null) {
+                    Location location = Location.viewTaskList(ListQuery.context, Id.NONE, mContext.getLocalId());
+                    mEventManager.fire(new NavigationRequestEvent(location));
+                }
+            } else {
+                if (mMultiSelector.getSelectedPositions().isEmpty() && mActionMode != null) {
+                    mActionMode.finish();
+                }
+            }
+        }
+
+        public void bindContext(Context context, SparseIntArray taskCountArray) {
+            mContext = context;
+            mContextListItem.setTaskCountArray(taskCountArray);
+            mContextListItem.updateView(context);
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            mActionMode = getRoboAppCompatActivity().startSupportActionMode(mEditMode);
+            mMultiSelector.setSelected(this, true);
+            return true;
+        }
+
+    }
+
+    public class ContextListAdapter extends AbstractCursorAdapter<ContextHolder> {
+
+        private SparseIntArray mTaskCountArray;
+
+        @Override
+        public ContextHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            ContextListItem listItem = mContextListItemProvider.get(getActivity());
+            return new ContextHolder(listItem);
+        }
+
+        @Override
+        public void onBindViewHolder(ContextHolder holder, int position) {
+            Log.d(TAG, "Binding holder at " + position);
+            Context context = readContext(position);
+            holder.bindContext(context, mTaskCountArray);
+        }
+
+        public Context readContext(int position) {
+            mCursor.moveToPosition(position);
+            return mContextPersister.read(mCursor);
+        }
+
+        public void setTaskCountArray(SparseIntArray taskCountArray) {
+            mTaskCountArray = taskCountArray;
+        }
+    }
 }
--- ShuffleAndroid/src/main/java/org/dodgybits/shuffle/android/list/view/project/ProjectListFragment.java
+++ ShuffleAndroid/src/main/java/org/dodgybits/shuffle/android/list/view/project/ProjectListFragment.java
@@ -2,7 +2,6 @@ package org.dodgybits.shuffle.android.list.view.project;
 
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.ActionMode;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
