addaa29dd4e039338f6b79ab2c910e10ad417bf3
Andy Bryant <andybryant@gmail.com>
2015-07-06 19:56:58 +1000

    first dodgy stab at coordinator layout

--- ShuffleAndroid/src/main/java/org/dodgybits/shuffle/android/list/activity/ProjectListActivity.java
+++ ShuffleAndroid/src/main/java/org/dodgybits/shuffle/android/list/activity/ProjectListActivity.java
@@ -1,5 +1,6 @@
 package org.dodgybits.shuffle.android.list.activity;
 
+import org.dodgybits.android.shuffle.R;
 import org.dodgybits.shuffle.android.core.activity.AbstractMainActivity;
 import org.dodgybits.shuffle.android.core.view.Location;
 
@@ -8,5 +9,10 @@ public class ProjectListActivity extends AbstractMainActivity {
     @Override
     public Location.LocationActivity getLocationActivity() {
         return Location.LocationActivity.ProjectList;
+    }
+
+    @Override
+    protected int contentView(boolean isTablet) {
+        return R.layout.entity_list_activity;
     }
 }
